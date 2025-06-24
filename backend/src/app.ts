075da5af231fca7a98a9f5b37fd842762e5a2dc2
Aman-Kakoria <aman.2022@vitstudent.ac.in>
2025-06-25 00:44:30 +0530

    shifted from local DB to cloud DB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const MONGO_URI = 'mongodb://localhost:27017/fittrack';
+const MONGO_URI = 'mongodb+srv://aman:amanaman07@cluster0.oq7jdad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const indexController = new IndexController();
 
